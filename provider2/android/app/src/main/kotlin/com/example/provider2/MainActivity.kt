a573c91ddbf0e9e25d1ca8f27d2e52a7cd56c5c1
suraj khot <surajkhot19082001@gmail.com>
2024-04-23 17:34:27 +0530

    ex1 updated

---
+++ provider2/android/app/src/main/kotlin/com/example/provider2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.provider2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ provider3/android/app/src/main/kotlin/com/example/provider3/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.provider3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
